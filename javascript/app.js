a244ed6d0cb2cf5740317431daa346df8b94779d
maram mustafa <maram.w.mustafa@gmail.com>
2021-03-25 15:51:15 +0200

    correct

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,16 @@
-alert("welcome in other community ")
+alert("welcome in other community ");
 
 
 
 var favAnimal = prompt('Choose your favorite', 'Dogs , Cats , Barids, monkys');
 if (favAnimal === 'Dogs'){
-document.write('<img src="https://rjeem.com/wp-content/uploads/2019/06/%D8%A7%D9%84%D9%83%D9%84%D8%A7%D8%A8.jpg">')
+document.write('<img src="https://rjeem.com/wp-content/uploads/2019/06/%D8%A7%D9%84%D9%83%D9%84%D8%A7%D8%A8.jpg">');
 } else if (favAnimal === 'Cats'){
-    document.write('<img src="https://lh3.googleusercontent.com/proxy/cRkHwOYYfSKN9DxhGAbJ7avCGTyElYEOYu2b3_j3jT3lQNkRtww4vPhy2E-EZP8wVBFgnUrs_OmSbY27L9hwQBS9DcAQd70aPlgEfvPYPuebKWw9">')
+    document.write('<img src="https://lh3.googleusercontent.com/proxy/cRkHwOYYfSKN9DxhGAbJ7avCGTyElYEOYu2b3_j3jT3lQNkRtww4vPhy2E-EZP8wVBFgnUrs_OmSbY27L9hwQBS9DcAQd70aPlgEfvPYPuebKWw9">');
 } else if (favAnimal === 'Barids'){
-    document.write('<img src="https://alshababradio.ps/upload/ar/images/1608643686-8427-11.jpg">')
+    document.write('<img src="https://alshababradio.ps/upload/ar/images/1608643686-8427-11.jpg">');
 } else if (favAnimal === 'Monkys'){
-    document.write('<img src="https://lh3.googleusercontent.com/proxy/k_R_73CKe-JIl7BfvI9c0MTtH936qR9ZiOhhqdNA9H97gKFUe-JvApAheOHz9KI5M22aIlhXUlMNm9vjiRNXjOkmrfVjLsHV1GTjZsWeqxsM7WGc6gCSivuvxG_CFpYDOnv82AGv08a4khkrYpKBbPZXrKFfyBR0">')
+    document.write('<img src="https://lh3.googleusercontent.com/proxy/k_R_73CKe-JIl7BfvI9c0MTtH936qR9ZiOhhqdNA9H97gKFUe-JvApAheOHz9KI5M22aIlhXUlMNm9vjiRNXjOkmrfVjLsHV1GTjZsWeqxsM7WGc6gCSivuvxG_CFpYDOnv82AGv08a4khkrYpKBbPZXrKFfyBR0">');
 } else {
-    alert('pick your favorite again please!')
+    alert('pick your favorite again please!');
 }
