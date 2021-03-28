8457f3857fc8ae5857ae8904facaa9c4c7a88de1
maram mustafa <maram.w.mustafa@gmail.com>
2021-03-28 16:33:46 +0300

    JS correction

--- javascript/app.js
+++ javascript/app.js
@@ -2,16 +2,48 @@ alert("welcome in other community ");
 
 
 
-var favAnimal = prompt('Choose your favorite', 'Dogs , Cats , Barids, monkys');
+var favAnimal = prompt('Choose your favorite', 'Dogs , Cats , Birds , monkys');
 alert ("you will see what you choose in the end of page")
 if (favAnimal === 'Dogs'){
 document.write('<img src="https://rjeem.com/wp-content/uploads/2019/06/%D8%A7%D9%84%D9%83%D9%84%D8%A7%D8%A8.jpg" hight= 500 width=500>')
 } else if (favAnimal === 'Cats'){
     document.write('<img src ="https://i.pinimg.com/originals/10/b9/e1/10b9e1376c80d06f1b33d4c79888c7f4.jpg" hight= 500 width=500>');
-} else if (favAnimal === 'Barids'){
+} else if (favAnimal === 'Birds'){
     document.write('<img src="https://alshababradio.ps/upload/ar/images/1608643686-8427-11.jpg" hight= 500 width=500>');
 } else if (favAnimal === 'Monkys'){
-    document.write('<img src="https://lh3.googleusercontent.com/proxy/k_R_73CKe-JIl7BfvI9c0MTtH936qR9ZiOhhqdNA9H97gKFUe-JvApAheOHz9KI5M22aIlhXUlMNm9vjiRNXjOkmrfVjLsHV1GTjZsWeqxsM7WGc6gCSivuvxG_CFpYDOnv82AGv08a4khkrYpKBbPZXrKFfyBR0" hight= 500 width=500>');
+    document.write('<img src="https://rjeem.com/wp-content/uploads/2019/10/289974-Sepik.jpg" hight= 500 width=500>');
 } else {
     alert('pick your favorite again please!');
 }
+
+
+var dogGame = prompt(' I have four legs. im very smart and i like play. i like smell things, Guess who I am ...');
+var dogName = 'Dog';
+
+while( dogGame !== dogName){
+    dogGame = prompt('please try again!')
+}
+
+
+
+var animalName = prompt('What is the animal you intend to adopt (cat, dog, monkey or bird)');
+var adaptionChoose = prompt('How many animals do you intend to adopt..?!');
+var image = '';
+
+for(var i = 1 ; i <=adaptionChoose; i++ ){
+    if(animalName === 'dog'){
+        image = image + '<img src="images/dog.jpg" hight= 500 width= 500 />';
+    }else if(animalName === 'cat'){
+        image = image + '<img src="images/cat.jpg" hight= 500 width= 500 />';
+    }else if(animalName === 'monky'){
+        image = image + '<img src="images/monky.jpg" hight= 500 width= 500 />';
+    }else if(animalName === 'bird'){
+        image = image + '<img src="images/birds.jpg" hight= 500 width= 500 />';
+    }
+}
+
+console.log(image);
+document.write(image);
+
+
+
